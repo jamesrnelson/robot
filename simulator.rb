a9ee0510521e4155c3d12faf20a4f0f42c424ce7
JNelson <nelson.jimmy@gmail.com>
2018-02-05 09:30:09 -0700

    initial commit

---
+++ simulator.rb
@@ -0,0 +1,12 @@
+class Robot
+  attr_reader :direction
+  def initialize(direction = nil)
+    @direction = direction
+  end
+
+  def orient(direction)
+  end
+
+  def bearing(direction)
+  end
+end
